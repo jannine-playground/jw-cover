d3ae702d5cf948a94737276ddaa74c88d38560f2
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-04-07 22:45:41 +0700

    update css

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ app.controller("HomeController", function($scope, JwInfos, $timeout){
       $scope.singles = datas;
 
       if(datas.length > 0){
-        $scope.selectSingle($scope.singles[0]);
+        random();
       }
 
       $timeout(function(){
@@ -22,11 +22,20 @@ app.controller("HomeController", function($scope, JwInfos, $timeout){
     });
   }
 
+  function random() {
+    var length = $scope.singles.length;
+    var index = Math.floor((Math.random() * length));
+    var single = $scope.singles[index];
+
+    $scope.selectSingle(single);
+    $scope.play(single);
+  }
+
   function initSwatch() {
-     $("#sb-container").swatchbook({
+     $scope.swatch = $("#sb-container").swatchbook({
       angleInc: 15,
       heighbor: 15,
-      initclosed: true,
+      initclosed: false,
       closeIdx:200
     });
   }
@@ -50,7 +59,7 @@ app.controller("HomeController", function($scope, JwInfos, $timeout){
   $scope.singles = [];
   $scope.selectedSingle = {};
   $scope.animate = false;
-
+  $scope.swatch = null;
 
   $scope.selectSingle = function(single, noAnimate){
     $scope.animate = false;
@@ -73,13 +82,16 @@ app.controller("HomeController", function($scope, JwInfos, $timeout){
     var stream = single.stream_url + "?client_id=" + JwInfos.clientId;
 
     if($scope.media != null) {
-
-//       console.log($scope.media);
       $scope.media.pause();
     }
 
     $scope.media = new Audio(stream);
     $scope.media.play();
+
+    $scope.media.addEventListener("ended", function(){
+      $scope.media.pause();
+      random();
+    })
 
     setInterval(function(){
       $scope.currentTime =$scope.media.currentTime;
